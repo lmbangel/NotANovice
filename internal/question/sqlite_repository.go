548036331ceaa02f803e81bbf0be9775b7604c3a
Lihle <mbangelalihle@gmail.com>
2025-09-20 06:11:30 +0200

    refactor prompt

--- internal/question/sqlite_repository.go
+++ internal/question/sqlite_repository.go
@@ -3,7 +3,9 @@ package question
 import (
 	"context"
 	"database/sql"
-	"fmt"
+	"encoding/json"
+	"os"
+	"strings"
 
 	"github.com/lmbangel/_novice/internal/db"
 	"github.com/lmbangel/_novice/pkg/agents"
@@ -18,32 +20,41 @@ func NewSQLiteQuestionRepository(db *sql.DB) QuestionRepository {
 }
 
 func (r *sqliteQuestionRepository) GenerateQuestion(ctx context.Context) (*Question, error) {
-	q := db.New(r.db)
+	data, err := os.ReadFile("./internal/question/_prompt.md")
+	if err != nil {
+		return nil, err
+	}
 
-	sysPrompt := `You are a discipleship leader (a shepherd).
-					You want your student to know the foundation and most important things of the faith of Christianity as commented on by the writer of the book of Hebrews (Hebrews 5:12-14).
-					Therefore, to test daily if your sheep/disciples are spending time learning and growing in the faith by particularly reading their Bible; have a quiz daily that is used to test the discipled for the day.
-					It's a straightforward biblical question every day. Either part of a scripture, or question on context of verse, or question about naming a verse that talks about a specific topic in a specific kind of context.
-					For example, How many times is Jesus recorded to be crying in the Bible.`
-
-	userPrompt := "What is the quiz of the day ?"
-
+	sysPrompt := string(data)
 	o := agents.Ollama{
 		Url: "http://localhost:11434/api/generate",
 		Request: &agents.Request{
 			Model:  "llama3.2:latest",
-			Prompt: fmt.Sprintf("%s %s", sysPrompt, userPrompt),
+			Prompt: sysPrompt,
 		},
 	}
 	response := o.Prompt()
 
+	resp := strings.TrimPrefix(strings.TrimSpace(response.Response), "```json")
+	resp = strings.TrimPrefix(resp, "```")
+	resp = strings.TrimSuffix(resp, "```")
+
+	var newQuestion Question
+	if err := json.Unmarshal([]byte(resp), &newQuestion); err != nil {
+		return nil, err
+	}
+
+	q := db.New(r.db)
 	qsn, _ := q.CreateQuestion(ctx, db.CreateQuestionParams{
-		Question:      response.Response,
-		CorrectAnswer: response.Response,
-		AAnswer:       response.Response,
-		BAnswer:       response.Response,
-		CAnswer:       response.Response,
-		DAnswer:       sql.NullString{},
+		Question:      newQuestion.Question,
+		CorrectAnswer: newQuestion.CorrectAnswer,
+		AAnswer:       newQuestion.AAnswer,
+		BAnswer:       newQuestion.BAnswer,
+		CAnswer:       newQuestion.CAnswer,
+		DAnswer: sql.NullString{
+			String: newQuestion.DAnswer.String,
+			Valid:  true,
+		},
 	})
 
 	return fmtQuesion(qsn), nil
